50bde1e266ffe048d3fbeb1a1ad68710039e0daf
Martin Rehberger <martin@mnkylab.com>
2018-04-23 14:27:16 +0200

    begin refactoring

--- acmeplugin.php
+++ acmeplugin.php
@@ -17,8 +17,9 @@
 namespace ACME\Demo;
 
 // If called directly, abort.
-if ( ! defined('WPINC'))
-  die;
+if ( ! defined('WPINC')) {
+	die;
+}
 
 // Global variables, as few as possible
 define('ACME_DEMO_VERSION', '1.0.0');
@@ -28,31 +29,33 @@ define('ACME_DEMO_BASENAME', plugin_basename(__FILE__));
 
 // autoloader
 spl_autoload_register(function($className){
-  $namespaceLength = strlen(__NAMESPACE__);
+	$namespaceLength = strlen(__NAMESPACE__);
 
-  // If not this plugins' namespace, abort.
-  if (substr($className, 0 , $namespaceLength) !== __NAMESPACE__)
-    return;
+	// If not this plugins' namespace, abort.
+	if (substr($className, 0 , $namespaceLength) !== __NAMESPACE__) {
+		return;
+	}
 
-  $className = ltrim(substr($className, $namespaceLength), '\\');
-  $fileName = sprintf('%s%s%s%s',
-    ACME_DEMO_PATH,
-    'src' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR,
-    str_replace('\\', DIRECTORY_SEPARATOR, $className),
-    '.php');
+	$className = ltrim(substr($className, $namespaceLength), '\\');
+	$fileName = sprintf('%s%s%s%s',
+		ACME_DEMO_PATH,
+		'src' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR,
+		str_replace('\\', DIRECTORY_SEPARATOR, $className),
+		'.php');
 
-  if (file_exists($fileName))
-    require_once $fileName;
+	if (file_exists($fileName)) {
+		require_once $fileName;
+	}
 });
 
 // activation
-register_activation_hook(__FILE__, array('\\ACME\\Demo\\Tool\\Activator', 'run'));
+register_activation_hook(__FILE__, array(Tool\Activator::class, 'run'));
 
 // deactivation
-register_deactivation_hook(__FILE__, array('\\ACME\\Demo\\Tool\\Deactivator', 'run'));
+register_deactivation_hook(__FILE__, array(Tool\Deactivator::class, 'run'));
 
 // uninstall 
-register_uninstall_hook(__FILE__, array('\\ACME\\Demo\\Tool\\Uninstaller', 'run'));
+register_uninstall_hook(__FILE__, array(Tool\Uninstaller::class, 'run'));
 
 // main plugin
-add_action('init', array('\\ACME\\Demo\\Plugin', 'init'));
+add_action('init', array(Plugin::class, 'init'));
