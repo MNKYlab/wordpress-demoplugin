ee65382c665938406a0ba8c7115c2d6cc3e67956
Martin Rehberger <martin@mnkylab.com>
2018-06-04 15:17:33 +0200

    psr-4 autoloading via composer

--- wordpress-demoplugin.php
+++ wordpress-demoplugin.php
@@ -14,48 +14,28 @@
  * Domain Path:	/languages
  */
 
-namespace ACME\Demo;
-
 // If called directly, abort.
 if ( ! defined('WPINC')) {
 	die;
 }
 
-// Global variables, as few as possible
+// global variables, as few as possible
 define('ACME_DEMO_VERSION', '1.0.0');
 define('ACME_DEMO_PATH', plugin_dir_path(__FILE__));
 define('ACME_DEMO_URL', plugin_dir_url(__FILE__));
 define('ACME_DEMO_BASENAME', plugin_basename(__FILE__));
 
-// autoloader
-spl_autoload_register(function($className){
-	$namespaceLength = strlen(__NAMESPACE__);
+// composer autoloading
+require_once ACME_DEMO_PATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-	// If not this plugins' namespace, abort.
-	if (substr($className, 0 , $namespaceLength) !== __NAMESPACE__) {
-		return;
-	}
-
-	$className = ltrim(substr($className, $namespaceLength), '\\');
-	$fileName = sprintf('%s%s%s%s',
-		ACME_DEMO_PATH,
-		'src' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR,
-		str_replace('\\', DIRECTORY_SEPARATOR, $className),
-		'.php');
-
-	if (file_exists($fileName)) {
-		require_once $fileName;
-	}
-});
-
-// activation
+// activation hook
 register_activation_hook(__FILE__, array(Tool\Activator::class, 'run'));
 
-// deactivation
+// deactivation hook
 register_deactivation_hook(__FILE__, array(Tool\Deactivator::class, 'run'));
 
-// uninstall 
+// uninstall hook
 register_uninstall_hook(__FILE__, array(Tool\Uninstaller::class, 'run'));
 
-// main plugin
+// start plugin execution
 add_action('plugins_loaded', array(Plugin::class, 'init'));
